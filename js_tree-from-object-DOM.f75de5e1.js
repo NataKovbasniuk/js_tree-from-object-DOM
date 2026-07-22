3724c763b2474b0f6340ee92b66e247bc80ac0d8
Nataliia <nata.kovbasnyukk@gmail.com>
2026-07-22 13:55:53 +0200

    make build

---
+++ js_tree-from-object-DOM.f75de5e1.js
@@ -0,0 +1,35 @@
+'use strict';
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+function createTree(element, data) {
+    const ul = document.createElement('ul');
+    for(const key in data){
+        const li = document.createElement('li');
+        li.textContent = key;
+        ul.appendChild(li);
+        if (typeof data[key] === 'object' && Object.keys(data[key]).length > 0) createTree(li, data[key]);
+    }
+    element.appendChild(ul);
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+    const root = document.createElement('div');
+    root.id = 'tree';
+    document.body.appendChild(root);
+    createTree(root, food);
+});
+
+//# sourceMappingURL=js_tree-from-object-DOM.f75de5e1.js.map
